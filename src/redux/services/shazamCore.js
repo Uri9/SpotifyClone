04ee3ce9a5b19e5a63eed6ca457c59afe60474e6
Uriel <019001186@upsrj.edu.mx>
2022-09-30 00:24:31 -0500

    Song Details Asus

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,18 +1,19 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const shazamCoreApi = createApi({
   reducerPath: 'shazamCoreApi',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://shazam-core.p.rapidapi.com/v1/',
     prepareHeaders: (headers) => {
-      headers.set('X-RapidAPI-Key', '42077518d2msh0b3df469ee3cf51p15aba3jsnf858d5e379f5');
+      headers.set('X-RapidAPI-Key', '42077518d2msh0b3df469ee3cf51p15aba3jsnf858d5e379f5')
 
-      return headers;
-    },
+      return headers
+    }
   }),
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => '/charts/world' }),
-  }),
-});
+    getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` })
+  })
+})
 
-export const { useGetTopChartsQuery } = shazamCoreApi;
+export const { useGetTopChartsQuery, useGetSongDetailsQuery } = shazamCoreApi
